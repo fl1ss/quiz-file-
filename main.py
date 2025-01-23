06d07285f6edfc7d91a85e6fda40b1bb265ced33
fl1ss <felicity_emery@biswashington.org>
2025-01-23 17:47:37 +0000

    some message

--- main.py
+++ main.py
@@ -1,1 +1,83 @@
+score = 0
 
+question = "What is the capital of England?"
+print(question)
+
+user_answer = input ("Your answer: ")
+
+if user_answer . lower() == "London":
+    print("Correct!")
+    score+= 1
+else:
+    ("Oops, the correct answer is London.")
+
+
+
+
+question = "What colour is grass?"
+print(question)
+
+user_answer = input ("Your answer: ")
+
+if user_answer . lower() == "Green":
+   print("Correct!")
+   score+= 1
+else:
+    ("Oops, the correct answer is Green.")
+
+
+
+
+question = "What colour is the biggest sea animal?"
+print(question)
+
+user_answer = input ("Your answer: ")
+
+if user_answer . lower() == "Blue whale":
+   print("Correct!")
+   score+= 1
+else: 
+    ("Oops, the correct answer is Blue whale.")
+ 
+
+
+
+question = "What colour are  strawberrys?"
+print(question)
+
+user_answer = input ("Your answer: ")
+
+if user_answer . lower() == "Red":
+   print("Correct!")
+   score+= 1
+else:
+    ("Oops, the correct answer is Red.")   
+
+
+
+question = "what colour are the poppy day poppys?"
+print(question)
+
+user_answer = input ("Your answer: ")
+
+if user_answer . lower() == "Red":
+   print("Correct!")
+   score+= 1
+else:
+    ("Oops, the correct answer is Red.")   
+
+
+
+
+question = "Where do penguins live?"
+print(question)
+
+user_answer = input ("Your answer: ")
+
+if user_answer.lower() == "South pole":
+   print("Correct!")
+   score+= 1
+else:
+    ("Oops, the correct answer is South pole.") 
+
+    print(f"Your score is: {score}")
